25e40a0b7cc62c4c2afe43e24ee99651ca14a4e1
Livia_Tassel <3459465562@qq.com>
2025-03-15 20:58:30 +0800

    2025-3-15

--- GPLT.cpp
+++ GPLT.cpp
@@ -4975,4 +4975,63 @@
 //     return 0;
 // }
 
+// 最长对称 SubString
+// #include <iostream>
+// #include <cstdio>
+// #include <cstring>
+// #include <string>
+// using namespace std;
+// string s;
+// int p[2010];
+// int main()
+// {
+//     getline(cin, s);
+//     int len = s.length(), id = 0, maxlen = 0;
+//     for (int i = len; i >= 0; --i)
+//     {
+//         s[i + i + 2] = s[i];
+//         s[i + i + 1] = '#';
+//     }
+//     s[0] = '*';
+//     for (int i = 2; i < 2 * len + 1; ++i)
+//     {
+//         if (p[id] + id > i)
+//             p[i] = p[2 * id - i] < p[id] + id - i ? p[2 * id - i] : p[id] + id - i;
+//         else
+//             p[i] = 1;
+//         while (s[i - p[i]] == s[i + p[i]])
+//             ++p[i];
+//         if (id + p[id] < i + p[i])
+//             id = i;
+//         if (maxlen < p[i])
+//             maxlen = p[i];
+//     }
+//     cout << maxlen - 1 << endl;
+//     return 0;
+// }
+
+// 犯二
+// #include <iostream>
+// #include <string>
+// #include <iomanip>
+// using namespace std;
+// int main()
+// {
+//     string n;
+//     cin >> n;
+//     double num = 0, length;
+//     n[0] == '-' ? length = n.length() - 1 : length = n.length();
+//     for (int i = 0; i < n.length(); i++)
+//     {
+//         if (n[i] == '2')
+//         {
+//             num++;
+//         }
+//     }
+//     n[0] == '-' ? num *= 1.5 : num *= 1;
+//     (n[n.length() - 1] - '0') % 2 == 0 ? num *= 2 : num *= 1;
+//     cout << fixed << setprecision(2) << num / length * 100 << "%" << endl;
+//     return 0;
+// }
+
 //
